99dba15ad6e7d860fc60029ca7700d1f161666b6
Bunyawat Singkiporn <pupahero@gmail.com>
2024-09-30 14:04:33 +0700

    exercise_1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let percentDiscount = 25 / 100;
+let totalDiscount = totalPrice * percentDiscount;
+let realPrice = totalPrice - totalDiscount;
+console.log(realPrice);
