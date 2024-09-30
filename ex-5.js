acdf68f671864ec810e7109d0dfa186251c07d7f
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-09-30 13:15:31 +0700

    complated exercise 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+const totalProductPrice = 27000;
+const discount = totalProductPrice * 0.25;
+const finalPrice = totalProductPrice - discount;
